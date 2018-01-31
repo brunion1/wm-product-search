856c19b4e1939cac558d3681988b931920476957
Runion, Beau <Beau.Runion@flooranddecor.com>
2018-01-30 19:14:41 -0500

    adding css back in

--- src/components/ItemSummary.js
+++ src/components/ItemSummary.js
@@ -26,7 +26,7 @@ class ItemSummary extends React.Component {
 
     render(){
         return(
-            <Panel onClick={this.viewItem} className={this.props.className}>
+            <Panel onClick={this.viewItem} className="item">
                 <Container>
                     <Row className="extra-margin item-name">
                         <Col xs={12} sm={12} md={12} lg={12} xl={12} className="mui--text-subhead">
