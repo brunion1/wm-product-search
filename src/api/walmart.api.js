a2ba0fb7782e6cf97a3f053ce4e07f368e1af411
Runion, Beau <Beau.Runion@flooranddecor.com>
2018-01-30 19:06:56 -0500

    adding css

--- src/api/walmart.api.js
+++ src/api/walmart.api.js
@@ -8,7 +8,7 @@ class WalmartAPI {
     apiKey = "gy2n8uu9mu5ndzwbedv4k8kk";
 
     search(query){
-        let url = `http://api.walmartlabs.com/v1/search?apiKey=${this.apiKey}&query=${query}`;
+        let url = `https://api.walmartlabs.com/v1/search?apiKey=${this.apiKey}&query=${query}`;
 
         return new Promise((resolve, reject) => {
             axios
@@ -26,7 +26,7 @@ class WalmartAPI {
     }
 
     getDetails(itemID){
-        let url = `http://api.walmartlabs.com/v1/items/${itemID}?apiKey=${this.apiKey}&format=json`;
+        let url = `https://api.walmartlabs.com/v1/items/${itemID}?apiKey=${this.apiKey}&format=json`;
         
         return new Promise((resolve, reject) => {
             axios
@@ -42,7 +42,7 @@ class WalmartAPI {
     }
 
     getRecommendations(itemID){
-        let url = `http://api.walmartlabs.com/v1/nbp?apiKey=${this.apiKey}&itemId=${itemID}`;
+        let url = `https://api.walmartlabs.com/v1/nbp?apiKey=${this.apiKey}&itemId=${itemID}`;
         
         return new Promise((resolve, reject) => {
             axios
